0c902606e6b4a005639db84360aa7923509ff373
rasteri <rasteri@gmail.com>
2018-12-13 10:09:43 +0000

    Added firmware hex, COPYING file

--- firmware/main.c
+++ firmware/main.c
@@ -1,4 +1,23 @@
-//#define _XTAL_FREQ 16000000
+/*
+ * Copyright (C) 2018 Andrew Tait <rasteri@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+ 
+ 
 #include <xc.h>
 #include "mcc_generated_files/mcc.h"
 
