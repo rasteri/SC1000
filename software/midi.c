c3af73dbf8fa5621410b49711ce65dcd22cde8f8
root <rasteri@gmail.com>
2019-12-29 00:23:39 +0000

    more work on MIDI hotplug

--- software/midi.c
+++ software/midi.c
@@ -22,6 +22,50 @@
 
 #include "midi.h"
 
+void listdev(char *devname)
+
+{
+
+    char** hints;
+    int    err;
+    char** n;
+    char*  name;
+    char*  desc;
+    char*  ioid;
+
+    /* Enumerate sound devices */
+    err = snd_device_name_hint(-1, devname, (void***)&hints);
+    if (err != 0) {
+
+        fprintf(stderr, "*** Cannot get device names\n");
+        exit(1);
+
+    }
+
+    n = hints;
+    while (*n != NULL) {
+
+        name = snd_device_name_get_hint(*n, "NAME");
+        desc = snd_device_name_get_hint(*n, "DESC");
+        ioid = snd_device_name_get_hint(*n, "IOID");
+
+        printf("Name of device: %s\n", name);
+        printf("Description of device: %s\n", desc);
+        printf("I/O type of device: %s\n", ioid);
+        printf("\n");
+
+        if (name && strcmp("null", name)) free(name);
+        if (desc && strcmp("null", desc)) free(desc);
+        if (ioid && strcmp("null", ioid)) free(ioid);
+        n++;
+
+    }
+
+    //Free hint buffer too
+    snd_device_name_free_hint((void**)hints);
+
+}
+
 // Get a list of midi devices, returns number of them
 // Dumb hack, just runs "amidi -l"
 int getmididevices(char names[32][32])
