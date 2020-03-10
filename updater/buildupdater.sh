a0456e4d1eaef82376051827986490f3fbd70897
root <rasteri@gmail.com>
2020-03-10 21:03:52 +0000

    updated installer

--- updater/buildupdater.sh
+++ updater/buildupdater.sh
@@ -5,3 +5,4 @@ cp /root/SC1000/software/xwax .
 tar -cf /root/SC1000/updater/sc.tar *
 cp /root/SC1000/updater/xwax "/media/root/NEW VOLUME"
 cp /root/SC1000/updater/sc.tar "/media/root/NEW VOLUME"
+cp /root/SC1000/software/scsettings.txt "/media/root/NEW VOLUME"
